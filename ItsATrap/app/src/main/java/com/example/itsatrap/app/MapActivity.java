7c9145c4aef3c57e2d5702f2ab8fea8aab68a83d
knipec <carissaknipe@gmail.com>
2014-05-08 14:34:06 -0500

    Added left drawer with hard-coded list of names

--- ItsATrap/app/src/main/java/com/example/itsatrap/app/MapActivity.java
+++ ItsATrap/app/src/main/java/com/example/itsatrap/app/MapActivity.java
@@ -2,17 +2,32 @@ package com.example.itsatrap.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
 public class MapActivity extends Activity {
 
+    private String[] drawerEntries;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        String[] hardCodedEntries = {"Jeff", "DSM", "Calder", "Carissa", "DermDerm", "Tao", "Carlton", "Quinn"};
+        drawerEntries = hardCodedEntries;
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, R.id.drawer_text_view, drawerEntries));
 //        // Get a handle to the Map Fragment
 //        GoogleMap map = ((MapFragment) getFragmentManager()
 //                .findFragmentById(R.id.map)).getMap();
